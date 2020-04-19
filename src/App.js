daf8a141277cc37aa4bae82454ad641fe53c32d4
codershona <falgunimst95@gmail.com>
2020-04-19 18:14:30 +0200

    Using CSS Transitions and Animations done

--- src/App.js
+++ src/App.js
@@ -12,26 +12,19 @@ class App extends Component {
 
   showModal = () => {
     this.setState({modalIsOpen: true});
-
   }
 
   closeModal = () => {
     this.setState({modalIsOpen: false});
-
   }
-
-  
 
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-
-         <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
-        <Backdrop show={this.state.modalIsOpen}/> 
-
-      <button className="Button" onClick={this.showModal}>Open Modal</button>
-
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
+        <Backdrop show={this.state.modalIsOpen} />
+        <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
       </div>
